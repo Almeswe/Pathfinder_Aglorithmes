2d1802d5a90f2bdba35546df86ec8afca66ead44
Almeswe <52577119+Almeswe@users.noreply.github.com>
2021-01-28 19:32:52 +0300

    Create bfs.py

---
+++ bfs.py
@@ -0,0 +1,33 @@
+from map import MapCell
+from queue import Queue
+
+from base import AlgorithmBase
+
+class BFS(AlgorithmBase):
+
+    def run(self):
+        frontier = Queue()
+        frontier.put(self.start_cell)
+        came_from = {}
+        came_from[self.start_cell] = None
+
+        while not frontier.empty():
+            current = frontier.get()
+            current.set_visited()
+
+            if current.equals(self.goal_cell):
+                self.draw_spath(current,came_from)
+                break
+
+            for next in self.window.win_map.get_nears(current):
+                if next.isBlocked():
+                    continue
+                next.set_front()
+                if next not in came_from:
+                    frontier.put(next)
+                    came_from[next] = current 
+            self.render()
+
+
+if __name__ == "__main__":
+    raise Exception("Bad entry point file, try main.py ...")
