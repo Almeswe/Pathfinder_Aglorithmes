c2fd2773a0d5f530f4d2daab2ec14e11fa08963b
Almeswe <52577119+Almeswe@users.noreply.github.com>
2021-01-28 19:32:54 +0300

    Update astar.py

--- astar.py
+++ astar.py
@@ -1,19 +1,9 @@
-from map import MapCell
 from math import sqrt,pow
 from queue import PriorityQueue
-from config import CELL_SIZE, WINDOW_SIZE
 
-class Astar():
+from base import AlgorithmBase
 
-    start = (0,0)
-    goal  = (WINDOW_SIZE[0]//CELL_SIZE-1,WINDOW_SIZE[1]//CELL_SIZE-1)
-
-    start_cell = MapCell(start[0],start[1],CELL_SIZE)
-    goal_cell  = MapCell(goal[0],goal[1],CELL_SIZE)
-
-    def __init__(self, pygame_window):
-        self.window = pygame_window
-        self.draw_goal_and_start()
+class Astar(AlgorithmBase):
 
     def run(self):
         frontier = PriorityQueue()
@@ -32,10 +22,7 @@ class Astar():
             current.set_visited()
 
             if current.equals(self.goal_cell):
-                while not self.start_cell.equals(came_from[current]):
-                    current.set_path()
-                    current = came_from[current]
-                self.window.render()
+                self.draw_spath(current,came_from)
                 break
 
             for next in self.window.win_map.get_nears(current):
@@ -52,14 +39,6 @@ class Astar():
 
     def heuristic(self, from_cell, to_cell):
         return sqrt(pow(from_cell.x - to_cell.x,2)+pow(from_cell.y - to_cell.y,2))
-
-    def draw_goal_and_start(self):
-        self.window.win_map.get_cell(self.start[0],self.start[1]).set_start() 
-        self.window.win_map.get_cell(self.goal[0],self.goal[1]).set_goal()
-        self.render()
-
-    def render(self):
-        self.window.render()
 
 if __name__ == "__main__":
     raise Exception("Bad entry point file, try main.py ...")
